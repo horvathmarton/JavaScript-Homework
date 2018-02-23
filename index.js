a4087bfae2d65dda2476f26b6a63637939f9128b
Marton Horvath <horvath.marton96@gmail.com>
2018-02-23 21:14:20 +0100

    Check password confirmation

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var IP = "0.0.0.0";
 
 app.use(express.static('static'));
 
+// Bocsánat, ennyit muszáj volt a szebb struktúra miatt
+app.get('/', function (req, res) {
+    res.redirect('/dashboard.html');
+});
+
 var server = app.listen(PORT, IP, function () {
     console.log('Running on port ' + PORT + '...');
 });
