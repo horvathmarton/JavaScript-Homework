533a5be69743ee1a95cded9bd75faa1be203a7b1
Marton Horvath <horvath.marton96@gmail.com>
2018-02-23 17:55:27 +0100

    Make header responsive

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 
 var PORT = 3000;
+var IP = "0.0.0.0";
 
 app.use(express.static('static'));
 
-var server = app.listen(PORT, function () {
+var server = app.listen(PORT, IP, function () {
     console.log('Running on port ' + PORT + '...');
 });
