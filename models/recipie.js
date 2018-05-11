f10b56f9c004573182d5cb90953d2d212e2b27d0
Marton Horvath <horvath.marton96@gmail.com>
2018-05-11 10:48:31 +0200

    Make recipie time constraint more permissive

--- models/recipie.js
+++ models/recipie.js
@@ -4,10 +4,7 @@ const db = require('../config/db');
 const Recipie = db.model('Recipie', {
     name: String,
     description: String,
-    time: {
-        type: String,
-        enum: ['15 perc', '30 perc', '45 perc', '1 óra', '1.5 óra', '2 óra']
-    },
+    time: Number,
     difficulty: {
         type: Number,
         enum: [1, 2, 3, 4, 5]
